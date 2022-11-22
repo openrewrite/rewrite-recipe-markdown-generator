27f4787689b3be163243cc0c9c0e220ecc7be8cd
Mike Solomon <mikesol@hey.com>
2022-11-22 13:39:48 -0800

    Create a CHANGELOG when building new recipes (#27)
    * Adds a new ChangedRecipe class which contains all the information about a recipe needed for the CHANGELOG
    * Updates the MarkdownRecipeArtifact class to have a map of recipe names to descriptors to assist in detection of changes
    * Updates the RecipeMarkdownGenerator class to build the CHANGELOG when run
    * Updates the recipeDescriptors to have the latest information with the latest style so this can work next time
    * An example of what this CHANGELOG would look like can be found here: https://gist.github.com/mike-solomon/16727159ec86ee0f0406ba389cbaecb1
    
    Co-authored-by: Mike Sol <mike-solomon@users.noreply.github.com>

--- src/main/kotlin/org/openrewrite/MarkdownRecipeArtifact.kt
+++ src/main/kotlin/org/openrewrite/MarkdownRecipeArtifact.kt
@@ -1,15 +1,14 @@
 package org.openrewrite
 
-import java.util.TreeSet
+import java.util.TreeMap
 
 data class MarkdownRecipeArtifact (
     val artifactId: String,
     val version: String,
-    val markdownRecipeDescriptors: TreeSet<MarkdownRecipeDescriptor>,
+    val markdownRecipeDescriptors: TreeMap<String, MarkdownRecipeDescriptor>,
 ) : Comparable<MarkdownRecipeArtifact> {
     override fun compareTo(other: MarkdownRecipeArtifact): Int {
         if (this.artifactId != other.artifactId) return this.artifactId.compareTo(other.artifactId)
-        if (this.version != other.version) return this.version.compareTo(other.version)
         if (this.markdownRecipeDescriptors != other.markdownRecipeDescriptors) return -1
         return 0
     }
