bc76061a1bd8d4adfee646236fdf3ea220c16ba8
Mike Solomon <mikesol@hey.com>
2023-02-15 11:41:50 -0800

    Add direct links to recipes (#43)
    * Add direct links to recipes
    
    Previously, we linked to the top-level Git repository from recipes.
    Now, we should link directly to the file that contains the recipe.
    
    This handles both imperative and declarative recipes as well as
    recipes from nested folders in rewrite.
    
    * Reduce map to if statement
    
    ---------
    
    Co-authored-by: Mike Sol <mike-solomon@users.noreply.github.com>

--- src/main/kotlin/org/openrewrite/RecipeOrigin.kt
+++ src/main/kotlin/org/openrewrite/RecipeOrigin.kt
@@ -10,7 +10,6 @@ class RecipeOrigin(
     val version: String,
     val jarLocation: URI
 ) {
-
     /**
      * The build plugins automatically have dependencies on the core libraries.
      * It isn't necessary to explicitly take dependencies on the core libraries to access their recipes.
@@ -18,27 +17,39 @@ class RecipeOrigin(
      */
     fun isFromCoreLibrary() = groupId == "org.openrewrite" && coreLibs.contains(artifactId)
 
-    fun githubUrl() =
+    private fun convertNameToJavaPath(recipeName: String): String =
+        recipeName.replace('.', '/') + ".java"
+
+    fun githubUrl(recipeName: String, source: URI): String {
+        val sourceString = source.toString()
+        var baseUrl = ""
+
         if (isFromCoreLibrary()) {
-            "https://github.com/openrewrite/rewrite"
-        } else if (artifactId == "rewrite-gradle") {
-            "https://github.com/openrewrite/rewrite/tree/main/rewrite-gradle"
+            baseUrl = "https://github.com/openrewrite/rewrite/blob/main/$artifactId/src/main"
         } else {
-            "https://github.com/openrewrite/$artifactId"
+            baseUrl = "https://github.com/openrewrite/$artifactId/blob/main/src/main"
         }
 
+        // YAML recipes will have a source that ends with META-INF/rewrite/something.yml
+        if (sourceString.substring(sourceString.length - 3) == "yml") {
+            val ymlPath = sourceString.substring(source.toString().lastIndexOf("META-INF"))
+            return "$baseUrl/resources/$ymlPath"
+        } else {
+            return baseUrl + "/java/" + convertNameToJavaPath(recipeName)
+        }
+    }
+
     fun issueTrackerUrl() =
-        if (artifactId == "rewrite-gradle") {
+        if (isFromCoreLibrary()) {
             "https://github.com/openrewrite/rewrite/issues"
         } else {
-            "${githubUrl()}/issues"
+            "https://github.com/openrewrite/$artifactId/issues"
         }
 
     companion object {
         private val parsePattern = Pattern.compile("([^:]+):([^:]+):([^:]+):(.+)")
         private val coreLibs = setOf(
-            "rewrite-core", "rewrite-java", "rewrite-java-11", "rewrite-java-8", "rewrite-xml",
-            "rewrite-maven", "rewrite-properties", "rewrite-yaml"
+            "rewrite-core", "rewrite-gradle", "rewrite-groovy", "rewrite-hcl", "rewrite-java", "rewrite-json", "rewrite-maven", "rewrite-properties", "rewrite-xml", "rewrite-yaml"
         )
 
         fun fromString(encoded: String): RecipeOrigin {
