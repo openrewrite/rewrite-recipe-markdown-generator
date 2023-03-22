64876895aaaf6d1b8533cb6c0345fb25a24ced8d
Mike Solomon <mike@moderne.io>
2023-03-22 07:55:21 -0700

    Add diff log generator (#48)
    Adds a way to create a new type of changelog that shows all of
    the recipes created since a specific version of OpenRewrite.
    
    Co-authored-by: Mike Sol <mike-solomon@users.noreply.github.com>

--- src/main/kotlin/org/openrewrite/MarkdownRecipeDescriptor.kt
+++ src/main/kotlin/org/openrewrite/MarkdownRecipeDescriptor.kt
@@ -6,7 +6,9 @@ data class MarkdownRecipeDescriptor (
     val name: String,
     val description: String,
     val docLink: String,
-    val options: TreeSet<RecipeOption>
+    val options: TreeSet<RecipeOption>,
+    val isImperative: Boolean = true,
+    val artifactId: String = "",
 ): Comparable<MarkdownRecipeDescriptor> {
     override fun compareTo(other: MarkdownRecipeDescriptor): Int {
         if (this.name != other.name) return this.name.compareTo(other.name)
