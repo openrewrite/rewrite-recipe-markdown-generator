9c0468744a428e251204d4be278a1d907e2402bd
Kun Li <kun@moderne.io>
2023-04-19 11:36:44 -0700

    upgrade com.fasterxml.jackson version and remove the suppression added for it before

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,8 +46,8 @@ dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
     implementation("info.picocli:picocli:latest.release")
     implementation("org.openrewrite:rewrite-core:$rewriteVersion")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.14.+")
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.14.+")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.+")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.15.+")
     runtimeOnly("org.slf4j:slf4j-simple:1.7.30")
 
     "recipe"(platform("org.openrewrite:rewrite-bom:$rewriteVersion"))
