080029a38734040d6a50699b282fed47bc0efc84
Mike Solomon <mike-solomon@users.noreply.github.com>
2023-11-07 09:34:39 -0800

    Clarify recipe sources

--- src/main/kotlin/org/openrewrite/RecipeMarkdownGenerator.kt
+++ src/main/kotlin/org/openrewrite/RecipeMarkdownGenerator.kt
@@ -774,7 +774,7 @@ class RecipeMarkdownGenerator : Runnable {
 
             writeln(
                 """
-                ## Source
+                ## Recipe source
                 
                 [GitHub](${
                     origin.githubUrl(
@@ -789,6 +789,17 @@ class RecipeMarkdownGenerator : Runnable {
                 
             """.trimIndent()
             )
+
+            if (recipeDescriptor.recipeList.size > 1) {
+                writeln(
+                    """
+                    {% hint style="info" %}
+                    This recipe is composed of more than one recipe. If you want to customize the set of recipes this is composed of, you can find and copy the GitHub source for the recipe from the link above.
+                    {% endhint %}
+                    """.trimIndent()
+                )
+            }
+
 
             // Options
             if (recipeDescriptor.options.isNotEmpty()) {
