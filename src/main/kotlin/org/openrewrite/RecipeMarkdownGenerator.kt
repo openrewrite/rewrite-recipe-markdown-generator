54bef7b32f53b54a1b3c0275fa2bac832282e99e
Mike Solomon <mike-solomon@users.noreply.github.com>
2024-12-19 10:46:20 -0800

    Minor rephrasing

--- src/main/kotlin/org/openrewrite/RecipeMarkdownGenerator.kt
+++ src/main/kotlin/org/openrewrite/RecipeMarkdownGenerator.kt
@@ -1977,7 +1977,7 @@ $cliSnippet
         dataTableCommandLineSnippet: String,
     ) {
         if (getLicense(origin) == License.Proprietary) {
-            writeln("This recipe has no required configuration options. Moderne users can run it via the Moderne CLI:")
+            writeln("This recipe has no required configuration options. Users of Moderne users run it via the Moderne CLI:")
         } else {
             writeln("This recipe has no required configuration options. " +
                     "It can be activated by adding a dependency on `${origin.groupId}:${origin.artifactId}` " +
