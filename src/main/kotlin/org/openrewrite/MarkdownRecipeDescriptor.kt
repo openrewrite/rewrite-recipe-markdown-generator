fff7d54dc5944f6ad5763e05c7ab272ba6474cd1
Sam Snyder <sam@moderne.io>
2025-01-24 21:44:43 +0000

    refactor: Common static analysis issues
    Use this link to re-run the recipe: https://app.moderne.io/recipes/org.openrewrite.staticanalysis.CommonStaticAnalysis?organizationId=T3BlblJld3JpdGU%3D
    
    Co-authored-by: Moderne <team@moderne.io>

--- src/main/kotlin/org/openrewrite/MarkdownRecipeDescriptor.kt
+++ src/main/kotlin/org/openrewrite/MarkdownRecipeDescriptor.kt
@@ -11,9 +11,15 @@ data class MarkdownRecipeDescriptor (
     val artifactId: String = "",
 ): Comparable<MarkdownRecipeDescriptor> {
     override fun compareTo(other: MarkdownRecipeDescriptor): Int {
-        if (this.name != other.name) return this.name.compareTo(other.name)
-        if (this.description != other.description) return this.description.compareTo(other.description)
-        if (this.options != other.options) return -1
+        if (this.name != other.name) {
+            return this.name.compareTo(other.name)
+        }
+        if (this.description != other.description) {
+            return this.description.compareTo(other.description)
+        }
+        if (this.options != other.options) {
+            return -1
+        }
         return 0
     }
 }
