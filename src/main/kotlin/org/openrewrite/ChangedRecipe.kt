016e173101a08e4453bb97b8c65df35172d1f63f
Sam Snyder <sam@moderne.io>
2025-01-28 08:37:46 -0800

    refactor: Common static analysis issues (#160)
    Use this link to re-run the recipe: https://app.moderne.io/recipes/org.openrewrite.staticanalysis.CommonStaticAnalysis?organizationId=T3BlblJld3JpdGU%3D
    
    Co-authored-by: Moderne <team@moderne.io>

--- src/main/kotlin/org/openrewrite/ChangedRecipe.kt
+++ src/main/kotlin/org/openrewrite/ChangedRecipe.kt
@@ -11,11 +11,21 @@ data class ChangedRecipe(
     val oldOptions: TreeSet<RecipeOption>?,
 ): Comparable<ChangedRecipe> {
     override fun compareTo(other: ChangedRecipe): Int {
-        if (this.artifactId != other.artifactId) return this.artifactId.compareTo(other.artifactId)
-        if (this.name != other.name) return this.name.compareTo(other.name)
-        if (this.description != other.description) return this.description.compareTo(other.description)
-        if (this.newOptions != other.newOptions) return -1
-        if (this.oldOptions != other.oldOptions) return -1
+        if (this.artifactId != other.artifactId) {
+            return this.artifactId.compareTo(other.artifactId)
+        }
+        if (this.name != other.name) {
+            return this.name.compareTo(other.name)
+        }
+        if (this.description != other.description) {
+            return this.description.compareTo(other.description)
+        }
+        if (this.newOptions != other.newOptions) {
+            return -1
+        }
+        if (this.oldOptions != other.oldOptions) {
+            return -1
+        }
         return 0
     }
 }
