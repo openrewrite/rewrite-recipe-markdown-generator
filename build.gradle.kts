3b646423cab720fb18aa28e08a6271694aafc085
Tim te Beek <tim@moderne.io>
2025-03-11 19:20:52 -0400

    OpenRewrite v8.48.0 (#174)
    * OpenRewrite v8.48.0
    
    * Drop recipes from modules where we only show version
    
    * Adopt actual release files in reference project
    
    * Remove entries from recipeDescriptors.yml

--- build.gradle.kts
+++ build.gradle.kts
@@ -145,16 +145,6 @@ application {
 }
 
 tasks.named<JavaExec>("run").configure {
-    // Additional modules whose versions we want to show, but not (yet) their recipes
-    dependencies {
-        "recipe"("org.openrewrite:rewrite-cobol:$rewriteVersion")
-        "recipe"("org.openrewrite:rewrite-csharp:$rewriteVersion")
-        "recipe"("org.openrewrite:rewrite-javascript:$rewriteVersion")
-        "recipe"("org.openrewrite:rewrite-polyglot:$rewriteVersion")
-        "recipe"("org.openrewrite:rewrite-python:$rewriteVersion")
-        "recipe"("org.openrewrite:rewrite-templating:$rewriteVersion")
-    }
-
     val targetDir = layout.buildDirectory.dir("docs").get().asFile
     // Collect all of the dependencies from recipeConf, then stuff them into a string representation
     val recipeModules = recipeConf.resolvedConfiguration.firstLevelModuleDependencies.flatMap { dep ->
