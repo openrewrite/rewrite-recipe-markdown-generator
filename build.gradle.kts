37112f737075c8b9ddf145c1b6e15b4a66319669
Tim te Beek <tim@moderne.io>
2025-06-27 12:46:39 +0200

    Exclude snapshot rewrite dependencies from rewrite-diffblue

--- build.gradle.kts
+++ build.gradle.kts
@@ -96,7 +96,10 @@ dependencies {
     "recipe"("org.openrewrite.recipe:rewrite-comprehension")
     "recipe"("org.openrewrite.recipe:rewrite-concourse")
     "recipe"("org.openrewrite.recipe:rewrite-cucumber-jvm")
-    "recipe"("org.openrewrite.recipe:rewrite-diffblue:latest.integration") // Not released yet
+    "recipe"("org.openrewrite.recipe:rewrite-diffblue:latest.integration") { // Not released yet
+        exclude(group = "org.openrewrite")
+        exclude(group = "org.openrewrite.recipe")
+    }
     "recipe"("org.openrewrite.recipe:rewrite-docker")
     "recipe"("org.openrewrite.recipe:rewrite-dotnet")
     "recipe"("org.openrewrite.recipe:rewrite-dropwizard")
