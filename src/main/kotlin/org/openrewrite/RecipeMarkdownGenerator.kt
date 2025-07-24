7f53a150844a5431a7c6b005f77a85fc16e59f07
Tim te Beek <tim@moderne.io>
2025-07-24 02:59:25 +0200

    OpenRewrite v8.59.1 (#203)
    * OpenRewrite v8.59.1
    
    * Fix links to recipe tags

--- src/main/kotlin/org/openrewrite/RecipeMarkdownGenerator.kt
+++ src/main/kotlin/org/openrewrite/RecipeMarkdownGenerator.kt
@@ -141,7 +141,7 @@ class RecipeMarkdownGenerator : Runnable {
     ): List<EnvironmentData> = runBlocking {
         println("Starting parallel recipe loading...")
         recipeOrigins.entries
-            .chunked(4) // Process in batches of 4 jars
+            .chunked(3) // Process in batches of jars
             .flatMap { batch ->
                 batch.map { recipeOrigin ->
                     async(Dispatchers.IO) {
@@ -1129,7 +1129,7 @@ import TabItem from '@theme/TabItem';
                         writeln("* [$tag](https://sonarsource.github.io/rspec/#/rspec/S${tag.substring(6)})")
                     } else {
                         val tagAnchor = tag.lowercase().replace(" ", "-")
-                        writeln("* [$tag](../recipes-by-tag#${tagAnchor})")
+                        writeln("* [$tag](/reference/recipes-by-tag#${tagAnchor})")
                     }
                 }
                 newLine()
