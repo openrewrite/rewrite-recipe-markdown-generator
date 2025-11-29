85f332b54c3369b3126ac4306c4321e615f79706
Tim te Beek <timtebeek@gmail.com>
2025-11-29 13:59:05 +0000

    AssertJ best practices
    Use this link to re-run the recipe: https://app.moderne.io/recipes/org.openrewrite.java.testing.assertj.Assertj?organizationId=QUxML01vZGVybmUvTW9kZXJuZSArIE9wZW5SZXdyaXRl
    
    Co-authored-by: Moderne <team@moderne.io>

--- src/test/kotlin/org/openrewrite/RecipeOriginTest.kt
+++ src/test/kotlin/org/openrewrite/RecipeOriginTest.kt
@@ -1,6 +1,6 @@
 package org.openrewrite
 
-import org.junit.jupiter.api.Assertions.assertEquals
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import java.net.URI
 
@@ -14,9 +14,6 @@ class RecipeOriginTest {
             "org.openrewrite.java.migrate.apache.commons.lang.ApacheCommonsStringUtilsRecipes\$AbbreviateRecipe",
             URI("https://github.com")
         )
-        assertEquals(
-            "https://github.com/openrewrite/rewrite-migrate-java/blob/main/src/main/java/org/openrewrite/java/migrate/apache/commons/lang/ApacheCommonsStringUtils.java",
-            githubUrl
-        )
+        assertThat(githubUrl).isEqualTo("https://github.com/openrewrite/rewrite-migrate-java/blob/main/src/main/java/org/openrewrite/java/migrate/apache/commons/lang/ApacheCommonsStringUtils.java")
     }
 }
