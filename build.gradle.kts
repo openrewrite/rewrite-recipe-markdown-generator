d03f6e066e27a8481bf77e7b5e4b0883795d4ae4
Tim te Beek <tim@moderne.io>
2026-02-25 22:18:24 +0100

    Remove completed TODO

--- build.gradle.kts
+++ build.gradle.kts
@@ -123,7 +123,6 @@ dependencies {
     "recipe"("org.openrewrite.recipe:rewrite-micrometer:$rewriteVersion")
     "recipe"("org.openrewrite.recipe:rewrite-micronaut:$rewriteVersion")
     "recipe"("org.openrewrite.recipe:rewrite-migrate-java:$rewriteVersion")
-    // TODO uncomment after first release
     "recipe"("org.openrewrite.recipe:rewrite-migrate-kotlin:$rewriteVersion")
     "recipe"("org.openrewrite.recipe:rewrite-migrate-python:$rewriteVersion")
     "recipe"("org.openrewrite.recipe:rewrite-netty:$rewriteVersion")
